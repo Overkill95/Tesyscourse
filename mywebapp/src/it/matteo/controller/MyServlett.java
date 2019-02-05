6a94bbaa1502f9d966e5eaef24e39b9aa8893bf4
Overkill95 <bla.matteo@hotmail.it>
2019-02-05 21:07:07 +0100

    primo servlett

---
+++ mywebapp/src/it/matteo/controller/MyServlett.java
@@ -0,0 +1,74 @@
+package it.matteo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.servlet4preview.RequestDispatcher;
+
+/**
+ * Servlet implementation class MyServlett
+ */
+@WebServlet("/prova1")
+public class MyServlett extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlett() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        response.setBufferSize(8192);
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html lang=\"en\">"
+                    + "<head><title>Servlet Hello</title></head>");
+
+            // then write the data of the response
+            out.println("<body  bgcolor=\"#ffffff\">"
+                + "<img src=\"duke.waving.gif\" "
+                + "alt=\"Duke waving his hand\">"
+                + "<form method=\"get\">"
+                + "<h2>Hello, my name is Duke. What's yours?</h2>"
+                + "<input title=\"My name is: \"type=\"text\" "
+                + "name=\"username\" size=\"25\">"
+                + "<p></p>"
+                + "<input type=\"submit\" value=\"Submit\">"
+                + "<input type=\"reset\" value=\"Reset\">"
+                + "</form>");
+
+            String username = request.getParameter("username");
+            if (username != null && username.length()> 0) {
+                RequestDispatcher dispatcher =
+                    (RequestDispatcher) getServletContext().getRequestDispatcher("/response");
+
+                if (dispatcher != null) {
+                    dispatcher.include(request, response);
+                }
+            }
+            out.println("</body></html>");
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
